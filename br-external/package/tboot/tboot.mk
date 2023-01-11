dbdb4a2356ff828ad68d4e6ec25a7919a4dd1aec
Randzio, Pawel <pawel.randzio@intel.com>
2023-01-11 10:22:28 +0100

    TBOOT Live Image v3.8.0
    Signed-off-by: Randzio, Pawel <pawel.randzio@intel.com>

--- br-external/package/tboot/tboot.mk
+++ br-external/package/tboot/tboot.mk
@@ -3,7 +3,7 @@
 # tboot
 #
 ################################################################################
-TBOOT_VERSION = v1.10.5
+TBOOT_VERSION = v1.11.0
 TBOOT_SITE = http://hg.code.sf.net/p/tboot/code
 TBOOT_SITE_METHOD = hg
 TBOOT_DEPENDENCIES = openssl
