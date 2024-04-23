1f041debac11d88738f829df034ab0f45d4d5e9d
MichalBrach <michalx.brach@intel.com>
2024-04-23 08:40:10 -0400

    hash update and fix tar (proper subfolders)
    Signed-off-by: MichalBrach <michalx.brach@intel.com>

--- br-external/package/change_link/change_link.sh
+++ br-external/package/change_link/change_link.sh
@@ -2,8 +2,12 @@
 
 path_to_dl="dl"
 path_to_dl_linux_firmware=$path_to_dl"/linux-firmware"
-path_to_dl_linux_firmware_next=$path_to_dl_linux_firmware"/os.linux.intelnext.firmware"
+path_to_dl_linux_firmware_next_parent=$path_to_dl_linux_firmware
+linux_firmware_next_filename="os.linux.intelnext.firmware"
+linux_firmware_next_proper_filename="linux-firmware-next"
+path_to_dl_linux_firmware_next=$path_to_dl_linux_firmware_next_parent"/"$linux_firmware_next_filename
 path_to_tar=$path_to_dl_linux_firmware/linux-firmware-next.tar.xz
+path_to_hash="package/linux-firmware/linux-firmware.hash"
 
 checkFirmwareNextExist()
 {
@@ -34,6 +38,10 @@ checkFirmwareNextExist()
     else
         echo $path_to_dl_linux_firmware' exist. Firmware next can be copied to build folder'
     fi
+
+    if [ ! -f $path_to_hash ]; then
+        echo "Error! File "$path_to_hash" does not exist. Skip..."
+    fi
 }
 
 makeTar()
@@ -41,7 +49,7 @@ makeTar()
     if [ ! -f $path_to_tar ]; then
         echo $path_to_tar' does not exist.'
         echo "tar cJf "$path_to_dl_linux_firmware/linux-firmware-next.tar.xz $path_to_dl_linux_firmware_next "(this may take a while)"
-        tar cJf $path_to_dl_linux_firmware/linux-firmware-next.tar.xz $path_to_dl_linux_firmware_next --checkpoint=.100
+        tar -C $path_to_dl_linux_firmware_next_parent  --transform="s/$linux_firmware_next_filename/$linux_firmware_next_proper_filename/" -cJf $path_to_tar $linux_firmware_next_filename --checkpoint=.100
     fi
 }
 
@@ -53,6 +61,77 @@ updateMakefile()
     sed -i "s|$line|$output|" package/linux-firmware/linux-firmware.mk
 }
 
+updateHshFile()
+{
+    line=$(grep $2 $path_to_hash)
+    sha256="$(sha256sum $1 | cut -d' ' -f1)"
+    filename=$(basename $1)
+    echo "sha256 " $sha256 " " $filename
+
+    output="sha256 "$sha256" "$filename
+    sed -i "s|$line|$output|" $path_to_hash  
+}
+
+updateHashTar()
+{
+    echo "Update Tar Hash"
+    updateHshFile $path_to_tar "linux-firmware-"
+}
+
+updateHashLicense()
+{
+    echo "Update License Hash"
+    tab=(
+        "LICENCE.Abilis"
+        "LICENSE.amdgpu"
+        "LICENCE.Marvell"
+        "LICENCE.atheros_firmware"
+        "ath10k/QCA6174/hw3.0/notice_ath10k_firmware-4.txt"
+        "ath10k/QCA6174/hw2.1/notice_ath10k_firmware-5.txt"
+        "ath10k/QCA6174/hw3.0/notice_ath10k_firmware-6.txt"
+        "LICENCE.broadcom_bcm43xx"
+        "LICENCE.chelsio_firmware"
+        "LICENCE.cypress"
+        "LICENCE.fw_sst_0f28"
+        "LICENCE.ibt_firmware"
+        "LICENSE.ice_enhanced"
+        "LICENCE.it913x"
+        "LICENCE.iwlwifi_firmware"
+        "LICENCE.microchip"
+        "LICENCE.moxa"
+        "LICENCE.qat_firmware"
+        "LICENCE.qla2xxx"
+        "LICENCE.ralink-firmware.txt"
+        "LICENCE.ralink_a_mediatek_company_firmware"
+        "LICENCE.rtlwifi_firmware.txt"
+        "LICENCE.ti-connectivity"
+        "LICENCE.xc4000"
+        "LICENCE.xc5000"
+        "LICENCE.xc5000c"
+        "LICENSE.QualcommAtheros_ar3k"
+        "LICENSE.QualcommAtheros_ath10k"
+        "LICENSE.dib0700"
+        "LICENSE.i915"
+        "LICENSE.qcom"
+        "LICENSE.radeon"
+        "LICENSE.sdma_firmware"
+        "WHENCE"
+        "qcom/NOTICE.txt"
+        "LICENCE.e100"
+    );
+
+    for element in "${tab[@]}"; do
+        updateHshFile $path_to_dl_linux_firmware_next"/"$element $element
+    done
+}
+
+updateHash()
+{
+    updateHashTar
+    updateHashLicense
+}
+
 checkFirmwareNextExist
 makeTar
 updateMakefile
+updateHash
