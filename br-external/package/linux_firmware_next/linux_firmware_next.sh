6772776ebaeb2c0a4da5d6f1136cd81b744178b5
Michalak, BartlomiejX <bartlomiejx.michalak@intel.com>
2024-07-03 04:56:03 -0400

    Add comments to refreshed script

--- br-external/package/linux_firmware_next/linux_firmware_next.sh
+++ br-external/package/linux_firmware_next/linux_firmware_next.sh
@@ -3,13 +3,18 @@
 FIRMWARE_PATH="os.linux.intelnext.firmware/"
 BUILD_PATH="../build/build/linux-firmware-next/i915"
 
+# Remove old i915 files from live image
 rm -rf $BUILD_PATH
 
+# Clone intelnext repo without downloading any files
 git clone --filter=blob:none --no-checkout https://github.com/intel-innersource/os.linux.intelnext.firmware
 
+# Set git to download only i915 folder
 git -C $FIRMWARE_PATH sparse-checkout set --no-cone /i915 '!*/i915'
 git -C $FIRMWARE_PATH checkout master
 
+# Copy i915 folder from intelnext repo to live image
 cp -r $FIRMWARE_PATH"i915" $BUILD_PATH
 
+# Remove intelnext repo
 rm -rf $FIRMWARE_PATH
