042c7982b2bbb5f8d130a05db04afca95f7a6e72
Pawlicki, AdamX <adamx.pawlicki@intel.com>
2025-04-22 02:48:50 -0400

    Tboot Live Image Intel Next release 3.9.9

--- br-external/package/tboot/tboot.mk
+++ br-external/package/tboot/tboot.mk
@@ -3,7 +3,7 @@
 # tboot
 #
 ################################################################################
-TBOOT_VERSION = v1.11.9
+TBOOT_VERSION = v1.11.10
 TBOOT_SITE = http://hg.code.sf.net/p/tboot/code
 TBOOT_SITE_METHOD = hg
 TBOOT_DEPENDENCIES = openssl
